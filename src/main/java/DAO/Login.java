7ae9caca56c889715927de82aec8a0c3895c71e8
Mahir Smajilbasic <mahir.smajilbasic@gmail.com>
2018-05-19 01:18:11 +0200

    Added basic login func, need to be developed more

---
+++ src/main/java/DAO/Login.java
@@ -0,0 +1,37 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import DTO.User;
+import db.ConnectionManager;
+
+public class Login {
+
+	private static Connection conn = ConnectionManager.getInstance().getConnection();
+
+	public static boolean login(User user) throws Exception {
+		String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+		ResultSet rs = null;
+
+		try (PreparedStatement stm = conn.prepareStatement(query)) {
+			stm.setString(1, user.getUsername());
+			stm.setString(2, user.getPassword());
+
+			((PreparedStatement) rs).executeQuery();
+			if (rs.next()) {
+				if (user.getUsername().equals(rs.getString("username"))
+						&& user.getPassword().equals(rs.getString("password"))) {
+					rs.close();
+					return true;
+				}
+			} else {
+				System.out.println("Login failed, please try again!");
+				return false;
+			}
+		} return true;
+
+	}
+
+}
